2205f9c7c67dd9ae9a8f7797865264134e55b431
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-08 16:13:16 +0300

    Create StartGameScreen.js

---
+++ Game/screens/StartGameScreen.js
@@ -0,0 +1,58 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.btnContainer}>
+        <View style={styles.btn}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.btn}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+export default StartGameScreen;
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  btnContainer: {
+    flexDirection: "row",
+  },
+  btn: {
+    flex: 1,
+  },
+});
