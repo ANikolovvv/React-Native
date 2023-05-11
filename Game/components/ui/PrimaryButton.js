5e9b54c0fd23a8c3d8dea7a0e7e2d6d94814dec8
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-11 19:39:45 +0300

    Create PrimaryButton.js

---
+++ Game/components/ui/PrimaryButton.js
@@ -0,0 +1,41 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+
+function PrimaryButton({ children,onPress }) {
+ 
+  return (
+    <View style={styles.outContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed ? [styles.container, styles.pressed] : styles.container
+        }
+        onPress={onPress}
+        android_ripple={{ color: Colors.primary600 }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  outContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  container: {
+    backgroundColor: Colors.primary500,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
