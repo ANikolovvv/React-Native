1a9bc0f40e82b8569a5494a3e5531e07d1b0c740
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-12 18:42:18 +0300

    Update App.js

--- Game/App.js
+++ Game/App.js
@@ -4,18 +4,32 @@ import { useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import GameScreen from "./screens/GameScreen";
 import Colors from "./constants/colors";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setOver] = useState(true);
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
+    setOver(false);
   }
+
+  function gameOverHandler() {
+    setOver(true);
+  }
+
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
   if (userNumber) {
-    screen = <GameScreen userNumber={userNumber}/>;
+    screen = (
+      <GameScreen userNumber={userNumber} gameIsOver={gameOverHandler} />
+    );
   }
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen />;
+  }
+
   return (
     <LinearGradient
       colors={[Colors.primary800, Colors.yellow]}
