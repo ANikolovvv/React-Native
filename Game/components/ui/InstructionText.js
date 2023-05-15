8726cf535417c411bc2026800c67128e54b018be
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-15 20:16:03 +0300

    Update InstructionText.js

--- Game/components/ui/InstructionText.js
+++ Game/components/ui/InstructionText.js
@@ -1,13 +1,14 @@
 import { Text, StyleSheet } from "react-native";
 import Colors from "../../constants/colors";
 
-function InstructionText({ children ,style}) {
-  return <Text style={[styles.instructionText,style]}>{children}</Text>;
+function InstructionText({ children, style }) {
+  return <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 export default InstructionText;
 
 const styles = StyleSheet.create({
   instructionText: {
+    fontFamily: "open-sans",
     color: Colors.yellow,
     fontSize: 24,
   },
