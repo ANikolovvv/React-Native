613b1ebbc86b13b1a50ccc604cf4e9da6a1439b8
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-23 18:56:21 +0300

    Create MealsOverviewScreen.js

---
+++ starting-project/screens/MealsOverviewScreen.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+//import { useRoute } from "@react-navigation/native";
+
+function MealsOverviewScreen({ route }) {
+  //const route = useRoute();
+  const categoryId = route.params.categoryId;
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>Meals Overview Screen - {categoryId}</Text>
+      </View>
+    </>
+  );
+}
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
