c409f219d37e350da4007f6664abb2e1c6db57ea
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-23 18:56:17 +0300

    Update CategoriesScreen.js

--- starting-project/screens/CategoriesScreen.js
+++ starting-project/screens/CategoriesScreen.js
@@ -1,24 +1,33 @@
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTitle from "../components/CategoryGridTitle";
 
-function render(itemData) {
-  return (
-    <CategoryGridTitle
-      title={itemData.item.title}
-      color={itemData.item.color}
-    />
-  );
-}
+function CategoriesScreen({ navigation }) {
+  function render(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
+    }
+    return (
+      <CategoryGridTitle
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
 
-function CategoriesScreen() {
   return (
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
       renderItem={render}
+      numColumns={2}
     />
   );
 }
 export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
