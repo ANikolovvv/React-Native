c5e50edb1188bb6cbff9855ee9f37be57f8045ee
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-26 21:36:59 +0300

    Create List.js

---
+++ starting-project/components/MealDetail/List.js
@@ -0,0 +1,27 @@
+import { Text, StyleSheet, View } from "react-native/types";
+
+function List({ data }) {
+  return data.map((ingredient) => (
+    <View style={styles.listItem}>
+      <Text style={styles.itemText} key={ingredient}>
+        {ingredient}
+      </Text>
+    </View>
+  ));
+}
+export default List;
+
+const styles = StyleSheet.create({
+  listItem: {
+    borderRadius: 6,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginVertical: 8,
+    marginHorizontal: 12,
+    backgroundColor: "#e2b497",
+  },
+  itemText: {
+    color: "#351401",
+    textAlign: "center",
+  },
+});
