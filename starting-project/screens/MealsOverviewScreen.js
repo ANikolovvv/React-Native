29011dbae6674df9db09eb16209f7f8e7ab67f1d
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-26 21:37:19 +0300

    Update MealsOverviewScreen.js

--- starting-project/screens/MealsOverviewScreen.js
+++ starting-project/screens/MealsOverviewScreen.js
@@ -1,19 +1,31 @@
+import { useLayoutEffect } from "react";
 import { View, FlatList, StyleSheet, Text } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 //import { useRoute } from "@react-navigation/native";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   //const route = useRoute();
   const categoryId = route.params.categoryId;
   const mealsArray = MEALS.filter((x) => {
     return x.categoryIds.indexOf(categoryId) >= 0;
   });
 
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId
+    ).title;
+    console.log(categoryTitle, "id");
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [categoryId, navigation]);
+
   function renderMeal(itemData) {
     return (
       <>
         <MealItem
+          id={itemData.item.id}
           title={itemData.item.title}
           url={itemData.item.imageUrl}
           duration={itemData.item.duration}
