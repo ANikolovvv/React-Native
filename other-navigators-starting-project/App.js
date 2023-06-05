ba0518bc0825037293ac1063e848b4922bb7e3b6
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-05 20:08:16 +0300

    Update App.js

--- other-navigators-starting-project/App.js
+++ other-navigators-starting-project/App.js
@@ -1,18 +1,45 @@
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+//import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+//import { createDrawerNavigator } from "@react-navigation/drawer";
 import WelcomeScreen from "./screens/WelcomeScreen";
-import UserScreen from "./screens/UserScreen"
+import UserScreen from "./screens/UserScreen";
+import { Ionicons } from "@expo/vector-icons";
 
-const Drawer = createDrawerNavigator();
+//const Drawer = createDrawerNavigator();
+const BottomTap = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator >
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-        <Drawer.Screen name="User" component={UserScreen} />
-      </Drawer.Navigator>
+      <BottomTap.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "3c0a6b",
+          },
+          headerTintColor: "white",
+          tabBarActiveTintColor:"3c0a6b",
+          //drawerStyle: { backgroundColor: "#ccc" },
+        }}
+      >
+        <BottomTap.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <BottomTap.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+      </BottomTap.Navigator>
     </NavigationContainer>
   );
 }
