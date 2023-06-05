920898e59e7c91927dee0e5c78da8d8c6ac48b8f
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-05 20:09:29 +0300

    Create FavoritesScreen.js

---
+++ starting-project/screens/FavoritesScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+function FavoritesScreen({}) {
+  return (
+    <View>
+      <Text>Favorites </Text>
+    </View>
+  );
+}
+export default FavoritesScreen;
