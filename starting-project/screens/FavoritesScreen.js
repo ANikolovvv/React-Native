5b7dcfeebe66b6d3e434ac9b699035987bdc7efd
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-07 20:09:13 +0300

    Update FavoritesScreen.js

--- starting-project/screens/FavoritesScreen.js
+++ starting-project/screens/FavoritesScreen.js
@@ -1,10 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+// import { useContext } from "react";
+// import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
+import { useDispatch, useSelector } from "react-redux";
 
 function FavoritesScreen({}) {
-  return (
-    <View>
-      <Text>Favorites </Text>
-    </View>
-  );
+  //const mealsList = useContext(FavoritesContext);
+  const favoriteMealsIds = useSelector((state) => state.favoriteMeals.ids);
+  const favoriteMeals = MEALS.filter((meal) => favoriteMealsIds.includes(meal.id));
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.root}>
+        <Text style={styles.text}>You have no favorite meals yet!</Text>
+      </View>
+    );
+  }
+  return <MealsList items={favoriteMeals} />;
 }
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
