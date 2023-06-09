e69c6afa2d2896de1183cd943617522adfc4b55f
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-09 19:30:32 +0300

    Create ManageExpenses.js

---
+++ Tracker/screens/ManageExpenses.js
@@ -0,0 +1,8 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function ManageExpenses() {
+  return <View><Text>Manage</Text></View>;
+}
+export default ManageExpenses;
+
+const styles = StyleSheet.create({});
