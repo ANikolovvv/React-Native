5fb4339ee8f9b3377d4d16a8f0bf671478246d7b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-09 19:30:16 +0300

    Create ExpensesSummary.js

---
+++ Tracker/components/Output/ExpensesSummary.js
@@ -0,0 +1,17 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function ExpensesSummary({ expenses, periodName }) {
+  const sum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
+
+  return (
+    <View>
+      <Text>{periodName}</Text>
+      <Text>${sum.toFixed(2)}</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
+const styles = StyleSheet.create({});
