4ce0dff28e2ddc1bc51c417760f2ce1c2a5fe399
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-10 09:39:42 +0300

    Create ExpenseItem.js

---
+++ Tracker/components/Output/ExpenseItem.js
@@ -0,0 +1,67 @@
+import { View, StyleSheet, Pressable, Text } from "react-native";
+
+import { GlobalStyles } from "../../constans/style";
+import { getFormattedDate } from "../../util/date";
+
+function ExpenseItem({ description, amount, date }) {
+  function expensePressHandler() {}
+
+  return (
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <View style={styles.item}>
+        <View>
+          <Text style={[styles.description, styles.text]}>{description}</Text>
+          <Text style={styles.text}>{getFormattedDate(date)}</Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amountText}>{amount.toFixed(2)}</Text>
+        </View>
+      </View>
+    </Pressable>
+  );
+}
+
+export default ExpenseItem;
+
+const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
+  item: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+    shadowColor: GlobalStyles.colors.gray500, //ios shadow
+    shadowRadius: 4, //ios shadow
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+  },
+  text: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  amountContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    minWidth: 80,
+  },
+  amountText: {
+    color: GlobalStyles.colors.primary500,
+    fontWeight: "bold",
+  },
+});
