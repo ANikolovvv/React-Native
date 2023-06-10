2f8f5eed7e95064847c27755c89c907fa18a444a
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-10 09:40:08 +0300

    Update RecentExpenses.js

--- Tracker/screens/RecentExpenses.js
+++ Tracker/screens/RecentExpenses.js
@@ -3,11 +3,7 @@ import { StyleSheet, View } from "react-native";
 import ExpensesOutput from "../components/Output/ExpensesOutput";
 
 function RecentExpenses() {
-  return (
-    <View>
-      <ExpensesOutput expensesPeriod={"Last 7 days."} />
-    </View>
-  );
+  return <ExpensesOutput expensesPeriod={"Last 7 days."} />;
 }
 export default RecentExpenses;
 
