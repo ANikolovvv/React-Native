e2b5b1945e8f36fce5447c12ca93c678fa76d88c
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-10 10:50:13 +0300

    Update ManageExpenses.js

--- Tracker/screens/ManageExpenses.js
+++ Tracker/screens/ManageExpenses.js
@@ -1,6 +1,16 @@
+import { useLayoutEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-function ManageExpenses() {
+function ManageExpenses({ route, navigation }) {
+  const expenseId = route.params?.expenseId;
+  const isEditing = !!expenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text>Manage</Text>;
 }
 export default ManageExpenses;
