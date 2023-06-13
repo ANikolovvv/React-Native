01491db0eb67155ff5408315601d1394dad71dfc
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-13 20:17:54 +0300

    Update RecentExpenses.js

--- Tracker/screens/RecentExpenses.js
+++ Tracker/screens/RecentExpenses.js
@@ -1,9 +1,25 @@
 import { StyleSheet, View } from "react-native";
 
 import ExpensesOutput from "../components/Output/ExpensesOutput";
+import { useContext } from "react";
+import { getDateMinusDate } from "../util/date";
+import { ExpensesContext } from "../store/expenses-context";
 
 function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod={"Last 7 days."} />;
+  const ctx = useContext(ExpensesContext);
+  const recent = ctx.expenses.filter((expense) => {
+    const today = new Date();
+    const sevenDays = getDateMinusDate(today, 7);
+
+    return expense.date >= sevenDays && expense.date <= today;
+  });
+  return (
+    <ExpensesOutput
+      expenses={recent}
+      expensesPeriod={"Last 7 days."}
+      fallbackText={"No expenses registered for the last 7 days."}
+    />
+  );
 }
 export default RecentExpenses;
 
