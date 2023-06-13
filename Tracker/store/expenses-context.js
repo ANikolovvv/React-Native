6009b97c0b108d681262538bf680aa5cc1f844e4
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-13 20:17:57 +0300

    Update expenses-context.js

--- Tracker/store/expenses-context.js
+++ Tracker/store/expenses-context.js
@@ -73,6 +73,16 @@ function ExpensesContextProvider({ children }) {
   function updateExpense(id, expenseData) {
     dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
   }
-  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+  const value = {
+    expenses: state,
+    addExpense: addExpense,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
+  };
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
 }
 export default ExpensesContextProvider;
