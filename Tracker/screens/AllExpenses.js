687567eb712eb54ab790c29aaf8fc8eb4ca873ae
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-13 20:17:48 +0300

    Update AllExpenses.js

--- Tracker/screens/AllExpenses.js
+++ Tracker/screens/AllExpenses.js
@@ -1,8 +1,17 @@
-import { StyleSheet, View, } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ExpensesOutput from "../components/Output/ExpensesOutput";
+import { useContext } from "react";
+import { ExpensesContext } from "../store/expenses-context";
 
 function AllExpenses() {
-  return <ExpensesOutput expensesPeriod={"Total"}/>;
+  const ctx = useContext(ExpensesContext);
+  return (
+    <ExpensesOutput
+      expenses={ctx.expenses}
+      expensesPeriod={"Total"}
+      fallbackText={"No registered expenses found!"}
+    />
+  );
 }
 export default AllExpenses;
 
