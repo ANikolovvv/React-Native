2ed9c5b8fa2ef9bee1b7e4cae57e76d4f853be76
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-17 16:18:15 +0300

    Update Input.js

--- Tracker/components/Manage/Input.js
+++ Tracker/components/Manage/Input.js
@@ -1,17 +1,21 @@
 import { View, Text, TextInput, StyleSheet } from "react-native";
 import { GlobalStyles } from "../../constans/style";
-import { multiply } from "react-native-reanimated";
 
-function Input({ label, style, textInput }) {
+function Input({ label, style, textInput, invalid }) {
   let inputStyles = [styles.input];
 
   if (textInput && textInput.multiline) {
     inputStyles.push(styles.inputMultiline);
   }
+  if (invalid) {
+    inputStyles.push(styles.invalidInput);
+  }
 
   return (
     <View style={[styles.inputContainer, style]}>
-      <Text style={styles.label}>{label}</Text>
+      <Text style={[styles.label, invalid && styles.invalidLabel]}>
+        {label}
+      </Text>
       <TextInput style={inputStyles} {...textInput} />
     </View>
   );
@@ -22,7 +26,6 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginHorizontal: 4,
     marginVertical: 8,
-
   },
   label: {
     fontSize: 12,
@@ -39,5 +42,11 @@ const styles = StyleSheet.create({
   inputMultiline: {
     minHeight: 100,
     textAlignVertical: "top",
+  },
+  invalidLabel: {
+    color: GlobalStyles.colors.error500,
+  },
+  invalidInput: {
+    backgroundColor: GlobalStyles.colors.error50,
   },
 });
