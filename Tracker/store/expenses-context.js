4aa25827900cd2096c429ba476f345c86c2d87ad
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-20 20:58:34 +0300

    Update expenses-context.js

--- Tracker/store/expenses-context.js
+++ Tracker/store/expenses-context.js
@@ -11,11 +11,10 @@ export const ExpensesContext = createContext({
 function reducer(state, action) {
   switch (action.type) {
     case "ADD":
-      const id = new Date().toString() + Math.random().toString();
-      return [{ ...action.payload, id: id }, ...state];
-
+      return [action.payload, ...state];
     case "SET":
-      return action.payload;
+      const inverted = action.payload.reverse();
+      return inverted;
     case "UPDATE":
       const findIndex = state.findIndex(
         (expense) => expense.id === action.payload.id
