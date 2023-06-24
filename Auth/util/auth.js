1d9ee6ba93557278cbe6d796ee5d5627f072c3ac
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-24 11:10:44 +0300

    Update auth.js

--- Auth/util/auth.js
+++ Auth/util/auth.js
@@ -2,10 +2,23 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyDkHHbeHuyPhm_SVhEvtsNaWxkz7K16U-w";
 
-export async function createUser(email, password) {
-  const res = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=` + API_KEY,
-    { email, password, returnSecureToken: true }
-  );
-  
+export async function auth(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const res = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+  const token = res.data.idToken;
+  console.log(res.data);
+  return token;
+}
+
+export function createUser(email, password) {
+  return auth("signUp", email, password);
+}
+
+export function login(email, password) {
+  return auth("signInWithPassword", email, password);
 }
