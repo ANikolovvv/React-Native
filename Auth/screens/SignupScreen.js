732a629976b52178d7c9f976275873315f8d656f
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-24 11:10:31 +0300

    Update SignupScreen.js

--- Auth/screens/SignupScreen.js
+++ Auth/screens/SignupScreen.js
@@ -3,14 +3,24 @@ import AuthContent from "../components/Auth/AuthContent";
 
 import { createUser } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
+import { useContext } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [isAuth, setIsAuth] = useState(false);
+  const ctx = useContext(AuthContext);
 
   async function signupHandler({ email, password }) {
     setIsAuth(true);
-    await createUser(email, password);
-    setIsAuth(false);
+    try {
+      const token = await createUser(email, password);
+      ctx.authenticate(token);
+    } catch (error) {
+      setIsAuth(false);
+      Alert.alert("Authentication failed!", "Could not create user.");
+    }
+
   }
 
   if (isAuth) {
