7000593a19155249426daf7aaf8c7c9e32af0bf0
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-24 11:10:25 +0300

    Update LoginScreen.js

--- Auth/screens/LoginScreen.js
+++ Auth/screens/LoginScreen.js
@@ -1,7 +1,31 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuth, setIsAuth] = useState(false);
+  const ctx = useContext(AuthContext);
+
+  async function loginHandler({ email, password }) {
+    setIsAuth(true);
+    try {
+      const token = await login(email, password);
+      ctx.authenticate(token);
+    } catch (error) {
+      Alert.alert("Authentication failed!", "Could not log in.");
+      setIsAuth(false);
+    }
+  }
+
+  if (isAuth) {
+    return <LoadingOverlay message={"Login user..."} />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
