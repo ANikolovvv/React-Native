badc12caea1ae60713123218c43ecaa9447ade4e
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-24 11:10:35 +0300

    Update WelcomeScreen.js

--- Auth/screens/WelcomeScreen.js
+++ Auth/screens/WelcomeScreen.js
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useContext } from "react";
+import { useEffect, useState } from "react";
+
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const ctx = useContext(AuthContext);
+
+  const token = ctx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-6cbd7-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=" +
+          token
+      )
+      .then((res) => {
+        setFetchedMessage(res.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +36,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
